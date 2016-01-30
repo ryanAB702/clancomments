8d6790324db0aafc3f1af24834f9fba73d3f3f94
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-01-29 22:54:27 -0500

    add filename to csv, clean up code, and set up tree walking

--- clancomments.py
+++ clancomments.py
@@ -8,57 +8,40 @@ start_dir = ""
 
 interval_regx = re.compile("(\x15\d+_\d+)")
 
+comments = []
 
 def parse_comments(clan_file):
     comments = []
     with open(clan_file, "rU") as file:
-        with open("test4.txt", "wb") as output:
-            last_buffer = None
+        filename = os.path.split(clan_file)[1]
+        last_buffer = None
 
-            curr_index = 0
-            for index, lines in enumerate(itertools.izip_longest(*[file]*7)):
-                # temp_comment = [None, None, None] # (index, timestamp, comment)
-                output.write("\nglob_index: {}\n".format(index))
+        curr_index = 0
+        for index, lines in enumerate(itertools.izip_longest(*[file]*7)):
+            for line_index, line in enumerate(lines):
+                temp_comment = [None, None, None, None] # (index, timestamp, comment)
+                if not line:
+                    continue
 
-                for line_index, line in enumerate(lines):
-                    temp_comment = [None, None, None] # (index, timestamp, comment)
-                    if not line:
-                        continue
-                    curr_index = 7*index+line_index
-                    output.write("real_index: {} ::   ".format(curr_index))
-                    output.write(line)
+                curr_index = 7*index+line_index
 
-                    if curr_index == 49084:
-                            print "hello"
+                if line.startswith("%com:") or line.startswith("%xcom:"):
+                    temp_comment[0] = filename
+                    temp_comment[1] = curr_index
+                    temp_comment[3] = line
+                    temp_buffer = lines[0:line_index]
 
-                    if line.startswith("%com:") or line.startswith("%xcom:"):
-                        temp_comment[0] = curr_index
-                        temp_comment[2] = line
-                        temp_buffer = lines[0:line_index]
+                    interval_string = reverse_interval_lookup(temp_buffer)
+                    if interval_string is None:
 
-                        output.write("curr_index: {}\ntemp_buffer: {}\n".format(curr_index, temp_buffer))
-                        print "curr_index: {}\ntemp_buffer: {}".format(curr_index, temp_buffer)
+                        interval_string = reverse_interval_lookup(last_buffer)
 
-
-
-                        interval_string = reverse_interval_lookup(temp_buffer)
-                        if interval_string is None:
-
-                            interval_string = reverse_interval_lookup(last_buffer)
-
-
-                        temp_comment[1] = interval_string
-                        output.write("temp_comment: {}\n".format(temp_comment))
-
-                        print "temp_comment: {}\n".format(temp_comment)
-
-                        comments.append(temp_comment)
-
-                last_buffer = lines
+                    temp_comment[2] = interval_string
+                    comments.append(temp_comment)
+            last_buffer = lines
     return comments
 
 def reverse_interval_lookup(buffer):
-    print "buffer_being_scanned: {}".format(buffer)
     temp_interval_string = None
     for i, element in reversed(list(enumerate(buffer))):
         interval_regx_result = interval_regx.search(element)
@@ -69,36 +52,38 @@ def reverse_interval_lookup(buffer):
                                                        .replace("\025", "")
             return temp_interval_string
 
-
 def output_comment_csv(comments):
-    with open("filtered_comments.csv", "wb") as file:
+    with open("filtered_comments_complete.csv", "wb") as file:
         writer = csv.writer(file)
-        writer.writerow(["line_num", "timestamp", "comment"])
+        writer.writerow(["filename", "line_num", "timestamp", "comment"])
         writer.writerows(comments)
 
 def filter_comments(comments):
     filtered_comments = []
 
     for comment in comments:
-        if "|" in comment[2]:
+        if "|" in comment[3]:
             continue
         else:
             filtered_comments.append(comment)
     return filtered_comments
 
 def walk_tree():
+    global comments
     for root, dirs, files in os.walk(start_dir):
-        if os.path.split(root)[1] == "Audio_Annotation":
-            for file in files:
-                if "_newclan_merged.cha" in file or "_final.cha" in file:
-                    print file
+        #if os.path.split(root)[1] == "Audio_Annotation":
+        for file in files:
+            if "_newclan_merged.cha" in file or "_final.cha" in file:
+                all_comments = parse_comments(os.path.join(root, file))
+                filtered_comments = filter_comments(all_comments)
+                comments += filtered_comments
 
 
 if __name__ == "__main__":
 
     start_dir = sys.argv[1]
 
-    #walk_tree()
-    all_comments = parse_comments("data/15_10_coderSM_final.cha")
-    filtered_comments = filter_comments(all_comments)
-    output_comment_csv(filtered_comments)
+    walk_tree()
+    # all_comments = parse_comments("data/15_10_coderSM_final.cha")
+    # filtered_comments = filter_comments(all_comments)
+    output_comment_csv(comments)
